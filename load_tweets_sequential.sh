4745aa8141e244b74686f7df633189c4f3b94283
ciherrera20 <ciherrera20@icloud.com>
2024-04-09 20:28:52 -0700

    Fixed bug in load_tweets.py. Now the sequential tests should pass

--- load_tweets_sequential.sh
+++ load_tweets_sequential.sh
@@ -1,26 +1,30 @@
 #!/bin/bash
 
 files=$(find data/*)
+pg_denormalized_port=13421
+pg_normalized_port=13422
+pg_normalized_batch_port=13423
+pg_denormalized_url="postgresql://postgres:pass@localhost:$pg_denormalized_port/postgres"
+pg_normalized_url="postgresql://postgres:pass@localhost:$pg_normalized_port/postgres"
+pg_normalized_batch_url="postgresql://postgres:pass@localhost:$pg_normalized_batch_port/postgres"
 
 echo '================================================================================'
 echo 'load denormalized'
 echo '================================================================================'
 time for file in $files; do
-    echo
-    # copy your solution to the twitter_postgres assignment here
+    ./load_tweets.py --db $pg_normalized_url --inputs $file
 done
 
 echo '================================================================================'
 echo 'load pg_normalized'
 echo '================================================================================'
 time for file in $files; do
-    echo
-    # copy your solution to the twitter_postgres assignment here
+    unzip -p $file | sed 's/\\u0000//g' | psql $pg_denormalized_url -c "COPY tweets_jsonb (data) FROM STDIN csv quote e'\x01' delimiter e'\x02';"
 done
 
 echo '================================================================================'
 echo 'load pg_normalized_batch'
 echo '================================================================================'
 time for file in $files; do
-    python3 -u load_tweets_batch.py --db=postgresql://postgres:pass@localhost:3/ --inputs $file
+    python3 -u load_tweets_batch.py --db=$pg_normalized_batch_url --inputs $file
 done
